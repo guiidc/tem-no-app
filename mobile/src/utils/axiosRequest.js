f3a9ce1a095bf3af1151be23875c3568ef4185c6
Guilherme C <82621852+guiidc@users.noreply.github.com>
2021-12-06 08:41:59 -0300

    Update axiosRequest.js

--- mobile/src/utils/axiosRequest.js
+++ mobile/src/utils/axiosRequest.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 Quando estava utlizando o axios com o endpoint localhost
 estava dando CONNECTION_REFUSED,
 então alterei o http://localhost:3001
-para http://MEU_IP_ATUAL:3001
+para http://MEU_IP_ATUAL:3001 e não obtive mais problemas,
+caso aconteça na rede local ai também é só alterar para o IP como no exemplo acima,
+estarei a disposição para qualquer dúvidas... Agradeço pela atenção
 
 */
 const axiosRequest = axios.create({
-  baseURL: 'http://10.0.0.104:3001',
+  baseURL: 'http://localhost:3001',
 });
 
 export default axiosRequest;
